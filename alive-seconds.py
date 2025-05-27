b64ed2334acf7e490e4742a504f5dcffe2f6203b
itspeciallist <159791752+itspeciallist@users.noreply.github.com>
2025-05-28 03:33:58 +0400

    Update alive-seconds.py

--- alive-seconds.py
+++ alive-seconds.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 
 def calculate_seconds_alive():
-    birth_date_str = input("Enter your birth date (YYYY-MM-DD): ")
+    birth_date_str = input("Sheikvanet dab tarighi (YYYY-MM-DD): ")
     
     try:
         birth_date = datetime.strptime(birth_date_str, "%Y-%m-%d")
         now = datetime.now()
         time_alive = now - birth_date
         seconds_alive = int(time_alive.total_seconds())
-        print(f"You have been alive for approximately {seconds_alive:,} seconds.")
+        print(f"tqven khart {seconds_alive:,} tsamis")
     except ValueError:
-        print("Invalid date format. Please use YYYY-MM-DD.")
+        print("arastsori formati. gtkhovt gamoikenot YYYY-MM-DD.")
 
 calculate_seconds_alive()
